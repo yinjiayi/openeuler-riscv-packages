977db407036e3c5b75da0d3be6327d4c36d4ef93
yinjiayi <12019860+yinjiayi@users.noreply.github.com>
2026-09-07 17:27:30 +0800

    fix(simple-mail): support target CMake toolchain

--- packages/simple-mail/tests/smoke.sh
+++ packages/simple-mail/tests/smoke.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
-rpm -q -- simple-mail
+rpm -q -- simple-mail simple-mail-devel
+test -e /usr/lib64/libSimpleMail3Qt5.so.0
+test -e /usr/lib64/libSimpleMail3Qt5.so.3.1.0
+test -e /usr/lib64/libSimpleMail3Qt5.so
+test -e /usr/include/simplemail3-qt5/SimpleMail/SimpleMail
+test -e /usr/lib64/cmake/SimpleMail3Qt5/SimpleMail3Qt5Config.cmake
+test -e /usr/lib64/pkgconfig/SimpleMail3Qt5.pc
